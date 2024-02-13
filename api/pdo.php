1fe9f6d5b56151a7eebb7f76ef591ce2ae79c7ff
crtnsj <crtnsj.pro@gmail.com>
2024-02-13 21:37:12 +0100

    restart

--- api/pdo.php
+++ api/pdo.php
@@ -1,4 +1,13 @@
 <?php
-class Pdo
-{
+$host = 'localhost'; // Hôte de la base de données
+$dbname = 'GStockB'; // Nom de la base de données
+$username = 'GStockB'; // Nom d'utilisateur de la base de données
+$password = 'Tsqa&r7PT&!pHc'; // Mot de passe de la base de données
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Définir le mode d'erreur de PDO à exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Impossible de se connecter à la base de données : " . $e->getMessage());
 }
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require("./vues/v_head.php");
-include("./vues/v_login.php");
+include("./vues/v_signin.php");
 require("./vues/v_foot.php");

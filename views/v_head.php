f6ac54b2c001e51f58f5c4e974d52a8cd6805b01
crtnsj <crtnsj.pro@gmail.com>
2024-02-27 11:33:42 +0100

    Ajout de Tailwind CSS

--- views/v_head.php
+++ views/v_head.php
@@ -6,10 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GStockB</title>
     <link rel="icon" href="./pics/favicon.ico" />
-    <link rel="stylesheet" href="/GStockB/styles/css-compiled.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+    <link href="./output.css" rel="stylesheet">
 </head>
 
 <body>

cdbe097fe832954b376b5cf7a8a2bb568f808188
crtnsj <crtnsj.pro@gmail.com>
2024-02-28 16:04:10 +0100

    suppression tailwind, ajout sass, style login page

--- views/user/v_login.php
+++ views/user/v_login.php
@@ -1,7 +1,20 @@
-<form action="./index.php?uc=user&action=validForm" method="post">
-    <p>Email :</p>
-    <input type="email" name="email" id="">
-    <p>Mot de passe :</p>
-    <input type="password" name="password" id="">
-    <input type="submit" value="Se Connecter">
-</form>
+<div class="loginPage">
+    <div class="loginBox">
+        <div class="loginForm">
+            <form action="./index.php?uc=user&action=validForm" method="post" class="loginForm--form">
+                <div>
+                    <p>Email :</p>
+                    <input type="email" name="email" id="" class="border border-black border-solid">
+
+                </div>
+                <div>
+                    <p>Mot de passe :</p>
+                    <input type="password" name="password" id="">
+                </div>
+                <input type="submit" value="Se Connecter">
+            </form>
+        </div>
+        <div class="loginIllu">
+            <div class="loginIllu--illu"></div>
+        </div>
+    </div>
--- views/v_head.php
+++ views/v_head.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GStockB</title>
     <link rel="icon" href="./pics/favicon.ico" />
-    <link href="./output.css" rel="stylesheet">
+    <link href="./styles/css-compiled.css" rel="stylesheet">
 </head>
 
 <body>

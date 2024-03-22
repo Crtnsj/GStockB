1071c8a3ef5336cf4d149ee9c1dc43b4723a389a
crtnsj <crtnsj.pro@gmail.com>
2024-03-22 22:17:11 +0100

    last updates

--- ressources/architecture.md
+++ ressources/architecture.md
@@ -1,17 +1,31 @@
 /GStockB
-    index.php
-    /Core
-        /App.php
-        /database.php
-    /Controllers
-        userController.php
-        orderController.php
-        stockController.php
-    /Models
-        User.php
-        Order.php
-        Stock.php
-    /Vues
-        /Order
-        /User
-        /Stock
+/public
+index.php
+/pics
+/styles
+/sass
+css-compiled.css
+/src
+/core
+/App.php
+/Database.php
+/controllers
+userController.php
+orderController.php
+stockController.php
+/models
+User.php
+Order.php
+Stock.php
+/views
+v_head.php
+v_home.php
+v_foot.php
+/Order
+/v_order.php
+...
+/User
+/v_user.php
+...
+/Stock
+v_stock.php
--- src/views/stock/v_stock.php
+++ src/views/stock/v_stock.php
@@ -1,4 +1,4 @@
-<a href="./index.php?uc=stock&action=create" class="btnAdd"><i class="ti ti-cube-plus"></i>Créer un stock</a>
+<?php echo $_SESSION['id_role'] < 3 ? "<a href='./index.php?uc=stock&action=create' class='btnAdd'><i class='ti ti-cube-plus'></i>Créer un stock</a>" : "" ?>
 
 <table class="table">
     <thead>
@@ -8,7 +8,7 @@
             <th><a href="./index.php?uc=stock&action=view&filter=description_st-<?php echo $column === 'description_st' && $order === 'ASC' ? 'DESC' : 'ASC'; ?>">Description<i class="ti ti-selector"></i></a></th>
             <th><a href="./index.php?uc=stock&action=view&filter=quantite_st-<?php echo $column === 'quantite_st' && $order === 'ASC' ? 'DESC' : 'ASC'; ?>">Quantité <i class="ti ti-selector"></i></a></th>
             <th><a href="./index.php?uc=stock&action=view&filter=type_st-<?php echo $column === 'type_st' && $order === 'ASC' ? 'DESC' : 'ASC'; ?>">Type <i class="ti ti-selector"></i></a></th>
-            <th>Actions</th>
+            <?php echo $_SESSION['id_role'] < 3 ? "<th>Actions</th>" : "" ?>
         </tr>
     </thead>
     <tbody>
@@ -32,10 +32,11 @@
                             ?>
                     </div>
                 </td>
+                <?php echo $_SESSION['id_role'] < 3 ? "
                 <td>
-                    <a href="./index.php?uc=stock&action=update&id=<?php echo $stock->id_st; ?>"><i class="ti ti-edit"></i></a>
-                    <a href="./index.php?uc=stock&action=delete&id=<?php echo $stock->id_st; ?>"><i class="ti ti-trash-x"></i></a>
-                </td>
+                    <a href='./index.php?uc=stock&action=update&id=<?php echo $stock->id_st; ?>'><i class='ti ti-edit'></i></a>
+                    <a href='./index.php?uc=stock&action=delete&id=<?php echo $stock->id_st; ?>'><i class='ti ti-trash-x'></i></a>
+                </td>" : "" ?>
             </tr>
         <?php } ?>
     </tbody>
